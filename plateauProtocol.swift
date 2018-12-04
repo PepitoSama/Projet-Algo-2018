cc0e4c9e3d0c9ad58610314ae550edf95717480c
Amjad Menouer <32568925+AmjadIG@users.noreply.github.com>
2018-12-05 00:57:11 +0100

    v2 de Plateau

--- plateauProtocol.swift
+++ plateauProtocol.swift
@@ -1,51 +1,42 @@
 import Foundation
 
-//Le type plateau est un quadruplet de données
+//Le type plateau est une collection qui va contenir 
 //itérateur ?
 protocol PlateauProtocol : Sequence{
-associatedtype Iterator : IteratorProtocol where Iterator.Element == String
+associatedtype IteratorCase : IteratorProtocol where IteratorCase.Element == String
 
 
-initPlateau : Int x Int x String x String-> Plateau
-Les paramètre donnés sont la coordonnées X et Y max respectivement
-Creer les joueurs avec les id 1 et 2 et les nom passé en paramère
-plateau() {
+// initPlateau : Int x Int x String x String-> Plateau
+// Les paramètre donnés sont la coordonnées X et Y max respectivement
+// Creer les joueurs avec les id 1 et 2 et les nom passé en paramère
+init()
 
-}
+// getCase : Plateau x Joueur x Joueur x int x int -> String
+// Renvoie sous forme de string la piece, si il y en a une, sur la case donnée en paramètre
+// La string renvoyée seras sous cette forme :
+// > Nom de la piece :
+// > coordonnée X : X coordonnée y : Y
+// > ooo
+// > o o
+// > ooo
+// Affiche des o en fonction des déplacements possibles de la piece
+// Affiche des x ou la piece ne peut pas se déplacer
+func getCase(joueur1: Joueur, joueur2: Joueur, coordX: Int, coordY: Int) -> String
 
-//Renvoie le joueur actif
-func getJoueurActif() -> Joueur
-
-//Renvoie le joueur passif
-func getJoueurPassif() -> Joueur
-
-//Modifie le joueur actif
-func setJoueurActif(newActif : Joueur) -> Bool
-
-//Modifie le joueur passif
-func setJoueurPassif(newPassif : Joueur) -> Bool
-
-// Le joueur actif devient passif
-// Le joueur passif devient actif
-// Renvoie true en cas de reussite
-// Renvoie false en cas d'echec
-func rotateJoueurs() -> Bool
-
-//getCase : Joueur x Joueur x int x int -> (Piece | Vide)
-//Renvoie la piece sur la case concernée
-//Renvoie Vide si il n'y a pas de pièce
-func getCase(joueur1: Joueur, joueur2: Joueur, coordX: Int, coordY: Int) -> Piece
-
-
-//validFromPosition : Joueur x Int x Int -> Boolean
+// validFromPosition : Plateau x Joueur x Int x Int -> Boolean
 // Renvoie True si le joueur possède une piece aux coordonnées indiquées
 func validFromPosition(joueurActif: Joueur, fromX: Int, fromY: Int) -> Bool
 
-// validToPosition : Int x Int x Int x Int -> Boolean
+// validToPosition : Plateau x Int x Int x Int x Int -> Boolean
 // Renvoie true si on peut déplacer la piece (aux coordonnées x1,y1) peut aller sur la case x2,y2
 func validToPosition(fromX: Int, fromY: Int, toX: Int, toY: Int) -> Bool
 
-// getPieceFromPosition : Int x Int -> (Piece | Vide)
-// Renvoie la piece sur la case donnée en paramètre
-// Renvoie Vide si aucune piece sur la position ou position inexistante
-func getPieceFromPosition() -> Piece?
+// getPieceFromPosition : Plateau x Int x Int -> Piece
+//
+func getPieceFromPosition(coordX: Int, coordY: Int) -> Piece
+
+// makeItPlateau : Plateau -> IteratorCase
+// Crée un itérateur pour la collection Plateau permettant de parcourir le plateau case par case
+func makeItPlateau() -> IteratorCase
+
+}
