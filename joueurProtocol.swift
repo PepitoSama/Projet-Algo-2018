350e72a7ca4b34ba49d57076a41a8e50e1caba7e
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-12-05 19:35:07 +0100

    update joueurProtocol

--- joueurProtocol.swift
+++ joueurProtocol.swift
@@ -1,30 +1,36 @@
 import Foundation
 
+//le type joueur va contenir en variable ( ou constante ) le nom du joueur ainsi que son numero 
+//et un objet reserve, contentant les objet piece étant dans la reserve du joueur courant 
+protocol JoueurProtocol : Sequence {
 
-protocol Joueur {
-
-
+//init() ->joueur
+//crée un joueur, avec un nom, et un numéro
+//pré 
+//données 
 init()
-//creation d'un joueur
 
 
+//getJoueur Piece -> Joueur 
+//récupere le joueur possédant la pièce passée en paramètre 
+//le renvoi
 func getJoueur(){} 
 
-/* Piece -> Joueur 
-récupère le joueur possèdant la pièce passée en paramètre.
-*/
-
+//setJoueur Joueur x Piece -> Joueur
+//attribut une piece à un joueur 
+//le renvoi
 func setJoueur(){}
 
-/* Joueur x Piece -> Joueur
-attribut une piece à un joueur ???
-*/
+//getJoueurActif Plateau -> Joueur 
+//récupère depuis un objet plateau le joueur "actif", soit le joueur jouant son tour 
+//le renvoi
+func getJoueurActif(){}
 
+//getName Joueur -> String
+//recupère le nom du joueur passé en paramètre et le renvoi sous forme de String 
+func getName(){}
+
+//capturer Joueur x Piece -> Joueur 
+//pré : la pièce en paramètre n'appartient pas au joueur 
+//post : la pièce est desormais dans la reserve du joueur passé en paramètre, elle n'est plus sur le plateau
 func capturer(){}
-
-/* Joueur x Piece ->
-données : joueur et piece 
-pré: la pièce passée en paramètre n'appartient pas au joueur, elle passe dans la 
-reserve du joueur
-post : la pièce n'est plus sur le plateau
-*/ 
