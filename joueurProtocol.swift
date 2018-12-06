6079df8908588aa0b64509116ece586422344d29
Amjad Menouer <32568925+AmjadIG@users.noreply.github.com>
2018-12-06 23:22:54 +0100

    Version terminées de joueur

--- joueurProtocol.swift
+++ joueurProtocol.swift
@@ -1,25 +1,31 @@
-import Foundation
+import Piece
+import Pieces
+import Plateau
+import Face
+import Reserve
+
 
 //Le type joueur va contenir en variable ( ou constante ) le nom du joueur ainsi que son numero
 //Et un objet reserve, contentant les objet piece étant dans la reserve du joueur courant
 protocol JoueurProtocol : Sequence {
+    associatedtype Reserve
 
-// creation d'un joueur
-// Un joueur à :
-// un nom
-// un id
-// Les pieces de base (accessible via Pieces) positionnée
-// Une reserve Vide
+// initJoueur : -> Joueur
+// Creation d'un joueur :
+// Un joueur à : un nom, un id, les pieces de base (accessibles via Pieces) positionnée, et une réserve vide
 init(name : String, id : int)
 
-// getName Joueur -> String
+// getName: Joueur -> String
 // recupère le nom du joueur et le renvoi sous forme de String
-func getName(){} -> String
+func getName() -> String
 
+// getReserve : Joueur -> Reserve
 // Renvoie la reserve du joueur
 // Pre : le joueur passé en paramètre possède une reserve
 func getReserve() -> Reserve
 
-// getId Joueur -> Int
+// getId : Joueur -> Int
 // renvoi l'id du joueur passé en paramètre
-func getId(joueur : Joueur) -> int
+// Post : l'id renvoyé est soit 1, soit 2
+func getId() -> Int
+}
