057184a6fe1a383d4c76fdbc5cfece9267726369
Amjad Menouer <32568925+AmjadIG@users.noreply.github.com>
2018-12-06 19:33:31 +0100

    fichier mis sous forme de Protocol

--- faceProtocol.swift
+++ faceProtocol.swift
@@ -1,39 +1,46 @@
-// Initialise une face
-// h pour Haut
-// hg pour Haut Gauche ...etc
-init(nom : String, h : Bool, b : Bool, g : Bool, d : Bool, hg : Bool, hd : Bool, bg : Bool, bd : Bool)
+import Foundation
 
-// Renvoie le nom de la face
-func getName() -> String
+  protocol FaceProtocol : Sequence {
 
-// haut : -> Bool
-// Renvoie true si la face peut aller en haut
-func haut() -> Bool
+  // initFace : -> Face
+  // Initialise une face
+  // h pour Haut
+  // hg pour Haut Gauche ...etc
+  init(nom : String, h : Bool, b : Bool, g : Bool, d : Bool, hg : Bool, hd : Bool, bg : Bool, bd : Bool)
 
-// bas : -> Bool
-// Renvoie true si la face peut aller en bas
-func bas() -> Bool
+  // getName : Face -> String
+  // Renvoie le nom de la face
+  func getName() -> String
 
-// gauche : -> Bool
-// Renvoie true si la face peut aller à gauche
-func gauche() -> Bool
+  // haut : Face -> Bool
+  // Renvoie true si la face peut aller en haut
+  func haut() -> Bool
 
-// droite : -> Bool
-// Renvoie true si la face peut aller à droite
-func droite() -> Bool
+  // bas : Face -> Bool
+  // Renvoie true si la face peut aller en bas
+  func bas() -> Bool
 
-// hDroite : -> Bool
-// Renvoie true si la face peut aller en haut à droite
-func hDroite() -> Bool
+  // gauche : Face -> Bool
+  // Renvoie true si la face peut aller à gauche
+  func gauche() -> Bool
 
-// hGauche : -> Bool
-// Renvoie true si la face peut aller en haut à gauche
-func hGauche() -> Bool
+  // droite : Face -> Bool
+  // Renvoie true si la face peut aller à droite
+  func droite() -> Bool
 
-// bDroite : -> Bool
-// Renvoie true si la face peut aller en bas à droite
-func bDroite() -> Bool
+  // hDroite : Face -> Bool
+  // Renvoie true si la face peut aller en haut à droite
+  func hDroite() -> Bool
 
-// bGauche : -> Bool
-// Renvoie true si la face peut aller en bas à gauche
-func bGauche() -> Bool
+  // hGauche : -> Bool
+  // Renvoie true si la face peut aller en haut à gauche
+  func hGauche() -> Bool
+
+  // bDroite : Face -> Bool
+  // Renvoie true si la face peut aller en bas à droite
+  func bDroite() -> Bool
+
+  // bGauche : Face -> Bool
+  // Renvoie true si la face peut aller en bas à gauche
+  func bGauche() -> Bool
+}
