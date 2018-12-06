42cf1c7b58028df28e24510d7f0cfdd8d0b37273
Amjad Menouer <32568925+AmjadIG@users.noreply.github.com>
2018-12-06 23:47:26 +0100

    Fini :3

--- piecesTest.swift
+++ piecesTest.swift
@@ -1,13 +1,15 @@
 //  piecesTest.swift
+import Piece
+import Joueur
+import Face
+import Plateau
+import Reserve
 
-import Foundation
-import Pieces
 @testable import piecesProtocol
-
 final class piecesTest: XCTestCase {
 
     // Test 1 : On teste le constructeur de Pieces
-  func testInit() {
+  func testInit() -> Bool {
     if let testInit = Pieces() {
         return true //le joueur a été créé
     }else {
@@ -15,9 +17,13 @@ final class piecesTest: XCTestCase {
     }
   }
 
-
-  func getPiece(nomPiece : String) -> Piece? {
-
+    // Test 2 : On test le getter qui peut récuperer une Piece dans la structure de donnée qui sera implémentée
+  func testGetPiece(nomPiece : String) -> Bool {
+    let testInit = Pieces()
+    if let testGetPiece = testInit.testGetPiece(nomPiece : String) {
+        return true //le joueur a été créé
+    }else {
+        return false
+    }
   }
-
 }
